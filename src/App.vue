5452c74f37c9ec61d0a95108b57308d026e2dda8
codeonium <codeon2020@gmail.com>
2021-04-10 14:41:46 +0100

    App modified v1.1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
   <div id="app">
 
-  </div>
+    <label for="civilization_select">Select a Civilization:</label>
+    <select id="civilization_select" v-model="selectedCivilization">
+      <option disabled value="">Select a civilization</option>
+      <option v-for="civilization in civilizations" :key="civilization.name" :value="civilization">{{civilization.name}}</option>
+    </select>
+
+    <civilization-detail v-if="selectedCountry" :selectedCountry="selectedCountry"/>
+
+    <button v-if="!favouriteCivs.includes(selectedCivilization)" v-on:click="addToFavourites">Add Civilization</button>
+
+    <favourite-civilizations :favouriteCs="favouriteCs"></favourite-countries>
+</div>
 </template>
 
 <script>
-
+import CivilizationDetail from './components/CivilizationDetail.vue';
+import FavouriteCiv from './components/FavouriteCiv.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      civilizations: [],
+      selectedCivilization: null,
+      favouriteCivilization: []
+    }
+  },
   components: {
-
+    'civilization-detail': CivilizationDetail,
+    'favourite-Civilizations': FavouriteCiv
+  },
+  mounted(){
+    this.getCivilizations()
+  },
+  methods: {
+    getCivilizations: function(){
+      fetch("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations")
+      .then(res => res.json())
+      .then(countries => this.countries = countries)
+    },
+    addToFavourites: function(){
+      this.favouriteCivs.push(this.selectedCivilization)
+    },
   }
 }
 </script>
